1070eae935d351c25df99703e8823f2b5ce0c772
David <grayd500@gmail.com>
2024-01-07 18:12:49 -0500

    update with starter code

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// client/src/App.jsx:
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
