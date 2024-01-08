1d849f05088525fc35508a3bdf2404a2d7174760
David <grayd500@gmail.com>
2024-01-08 14:21:13 -0500

    Update with linkage to GoogleBooks for books in browser

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -59,9 +59,15 @@ const SavedBooks = () => {
         {userData.savedBooks.map((book) => (
           <Col md="4" key={book.bookId}>
             <Card border='dark'>
-              {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              {book.image && (
+                <a href={book.link} target="_blank" rel="noopener noreferrer">
+                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                </a>
+              )}
               <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
+                <Card.Title>
+                  <a href={book.link} target="_blank" rel="noopener noreferrer">{book.title}</a>
+                </Card.Title>
                 <p className='small'>Authors: {book.authors.join(', ')}</p>
                 <Card.Text>{book.description}</Card.Text>
                 <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
