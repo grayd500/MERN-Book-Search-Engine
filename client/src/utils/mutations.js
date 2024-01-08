a882c1d7eecdcc0e708e3ffd92c34a61bba654b6
David <grayd500@gmail.com>
2024-01-07 22:51:27 -0500

    Search functionality updated and known working in browser

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,4 +14,50 @@ export const LOGIN_USER = gql`
   }
 `;
 
-// Add other mutations like ADD_USER, SAVE_BOOK, REMOVE_BOOK similarly
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
