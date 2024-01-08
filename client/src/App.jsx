f67e61287e97f3ff129a58f018320170f14ab704
David <grayd500@gmail.com>
2024-01-07 19:01:27 -0500

    Update with schema data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 // client/src/App.jsx:
-import './App.css';
-import { Outlet } from 'react-router-dom';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
