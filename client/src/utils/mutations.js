d07c06dbd7c7cc24b0c5b0cf227d6c172593b523
David <grayd500@gmail.com>
2024-01-07 19:08:36 -0500

    update with code for pages and utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+// client/src/utils/mutations.js:
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Add other mutations like ADD_USER, SAVE_BOOK, REMOVE_BOOK similarly
